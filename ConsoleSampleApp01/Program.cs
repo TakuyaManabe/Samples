4a1ae905a359ad55a97c2ea8cf3c6b30b0a8b5ff
Takuya Manabe <manabe@nec.com>
2021-10-12 12:08:23 +0900

    とりあえずコミット

---
+++ ConsoleSampleApp01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSampleApp01
+{
+    /// <summary>
+    /// コンソールアプリサンプル０１
+    /// 標準入力・標準出力・標準エラー出力
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
