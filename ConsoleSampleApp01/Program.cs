c685de73573c26883f4eaeac63f1d327cca5600f
Takuya Manabe <manabe@nec.com>
2021-10-12 12:20:27 +0900

    標準入力・標準出力・標準エラー出力を追加

--- ConsoleSampleApp01/Program.cs
+++ ConsoleSampleApp01/Program.cs
@@ -12,8 +12,20 @@ namespace ConsoleSampleApp01
     /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// メイン関数
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
         static void Main(string[] args)
         {
+            // 標準入力から１行読み込む
+            var inputText = Console.ReadLine();
+
+            // 標準出力へ入力したテキストを出力する
+            Console.WriteLine($"入力されたテキスト = {inputText}");
+
+            // 標準エラー出力へ入力された文字数を出力する
+            Console.Error.WriteLine($"入力された文字数 = {inputText.Length}");
         }
     }
 }
